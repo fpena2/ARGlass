d713ff0273151110f0327050851c73893e6ee183
Franzel P <33009908+fpena2@users.noreply.github.com>
2018-05-09 10:30:26 -0400

    Update displayDriver.ino

--- arduino/displayDriver.ino
+++ arduino/displayDriver.ino
@@ -1,29 +1,33 @@
 // Display Setup
 #include <Wire.h> 
 #include <SFE_MicroOLED.h>  
+
+// Communication Setup
+#include <SoftwareSerial.h>  
+int bluetoothRx = 2;  // RX-I 
+int bluetoothTx = 3;  // TX-O 
+
+SoftwareSerial bluetooth(bluetoothRx, bluetoothTx );
+
+
+// Screen
 #define PIN_RESET 9  
 #define PIN_DC    8 
 #define PIN_CS    10 
 #define DC_JUMPER 1
 
-MicroOLED oled(PIN_RESET, DC_JUMPER); 
-
-// Communication Setup
-#include <SoftwareSerial.h>  
-int bluetoothTx = 3;  // TX-O 
-int bluetoothRx = 2;  // RX-I 
-
-SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
+MicroOLED oled(PIN_RESET, DC_JUMPER);
 
 
 void setup(){
   // Display
   oled.begin();
   oled.clear(PAGE);
-  oled.clear(ALL);  
+  oled.clear(ALL); 
 
   //Bluetooth
   Serial.begin(9600); 
+  //115200
   bluetooth.begin(115200);  
   bluetooth.print("$");  // Print three times individually
   bluetooth.print("$");
@@ -34,14 +38,21 @@ void setup(){
 }
 
 void loop(){
+    
     if(bluetooth.available()){
-      Serial.print((char)bluetooth.read());  
-      oled.setFontType(0);
-      oled.setCursor(0,0);
-      oled.print((char)bluetooth.read()); 
-   }
-
-      oled.display();
-      oled.clear(PAGE);
+      Serial.print((char)bluetooth.read());
+     // oled.print((char)bluetooth.read()); 
+      
+      oled.setFontType(1);  // Set font to type 1
+      oled.clear(PAGE);     // Clear the page
+      oled.setCursor(2, 0); // Set cursor to top-left
+      
+      // Print can be used to print a string to the screen:
+      oled.print((String)bluetooth.readString());
+      
+      oled.display();       // Refresh the display
       delay(1000);
+        
+      oled.clear(PAGE);  
+   } 
  }
