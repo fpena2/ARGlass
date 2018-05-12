7bdb1ee7aa5fe90bfd432ff75d87a8942fb53759
Franzel P <33009908+fpena2@users.noreply.github.com>
2018-05-12 16:55:15 -0400

    Update serialSend.py

--- processor/serialSend.py
+++ processor/serialSend.py
@@ -1,12 +1,13 @@
 from bluetooth import *
 import VideoProcessor
 
-
 def connect():
-    socket = BluetoothSocket(RFCOMM)
+    socket = BluetoothSocket(RFCOMM) # starts a bluetooth socket
+    # connects to our RN-42 on port number 1
     socket.connect(("00:06:66:6B:B6:7A", 1))
 
     print("connected...")
+    # call function to process video and send data to module
     VideoProcessor.getScreenData(socket)
     socket.close()
 
