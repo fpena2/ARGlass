16a7d9656e4fc8d0e9c259cc87b961674237307a
Franzel P <33009908+fpena2@users.noreply.github.com>
2018-05-12 16:49:03 -0400

    Update VideoProcessor.py

--- processor/VideoProcessor.py
+++ processor/VideoProcessor.py
@@ -3,17 +3,18 @@ import cv2
 from moviepy.editor import VideoFileClip
 import string
 
-
+# Function to process a video file frame-by-frame
 def getScreenData(socket=None):
-    videoFrame = VideoFileClip('f2.mp4')#.subclip(1,5)
-    flag = "--psm 6"
+    videoFrame = VideoFileClip('f2.mp4') # loads file located in current dir 
+    flag = "--psm 6"  # Tesseract flag to detect digits
     redValue_prev = "-1"
     blueValue_prev = "-1"
 
     for aFrame in videoFrame.iter_frames(fps = 4):
         aFrame = cv2.cvtColor(aFrame, cv2.COLOR_RGB2GRAY)
 
-        """[yPixel: yPixel + height, xPixel: xPixel + width]"""
+        """[yPixel: yPixel + height, xPixel: xPixel + width]
+        Coordinates used for CBS Sports"""
         bluescore_img = aFrame[595:595 + 37, 431: 431 + 55]
         redscore_img = aFrame[593: 593 + 43, 624: 624 + 51]
         blue_img = aFrame[597: 597 + 32, 362: 362 + 70]
